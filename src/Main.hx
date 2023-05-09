b875bcb4751e5e3fca0f222c51c3e089b1d150a1
cowwabanga <114105506+cowwabanga@users.noreply.github.com>
2023-05-09 17:58:28 +0500

    FIX: Oops, forgot to ignore bin/ folder

--- src/Main.hx
+++ src/Main.hx
@@ -3,7 +3,7 @@ import hxd.res.DefaultFont;
 class Main extends hxd.App {
     override function init() {
         var tf = new h2d.Text(DefaultFont.get(), s2d);
-        tf.text = "Hello Heaps.io!";
+        tf.text = "Hello World!";
     }
 
     static function main() {
